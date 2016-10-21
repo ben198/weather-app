37dde7a053a1c147a8ef4b8b99ebccf8b67de67e
Ben Townsend <btownsend220@gmail.com>
2016-10-21 16:41:00 +0100

    comments and real coordinates added

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-const apiKey = 'apiKey';
-const lat = 12.345678;
-const lon = -1.234567;
+const apiKey = 'API_KEY_GOES_HERE';
+// coordinates for Nuffield Industrial Estate, Poole, England
+const lat = 50.740792;
+const lon = -1.979158;
 
 export { apiKey, lat, lon };
