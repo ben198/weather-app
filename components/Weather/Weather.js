04507b0f758f9f182b1280da70b42d1ea2096062
Ben Townsend <btownsend220@gmail.com>
2016-10-21 16:20:53 +0100

    react-skycons added to graphically illustrate current weather

--- components/Weather/Weather.js
+++ components/Weather/Weather.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import superagent from 'superagent';
 import jsonp from 'superagent-jsonp';
 import { apiKey, lat, lon } from '../../config';
+import Skycons from 'react-skycons';
 
 function fahrenheitToCensius(temp) {
     return (temp - 32) * 5 / 9;
+}
+
+function transformIconText(text) {
+    return text.split('').map((char) => {
+        if (char === '-') {
+            return '_';
+        } else {
+            return char.toUpperCase();
+        }
+    }).join('');
 }
 
 export default class Weather extends Component {
@@ -15,7 +26,8 @@ export default class Weather extends Component {
             forecast: {
                 currently: {
                     temperature: null,
-                    summary: null
+                    summary: null,
+                    icon: 'PARTLY_CLOUDY_DAY'
                 }
             }
         }
@@ -39,9 +51,16 @@ export default class Weather extends Component {
     render() {
         let tempInFahrenheit = this.state.forecast.currently.temperature;
         let tempInCelsius = fahrenheitToCensius(tempInFahrenheit);
+        let iconText = transformIconText(this.state.forecast.currently.icon);
         return (
             <div>
                 <h2>Weather at Nuffield Industrial Estate</h2>
+                <div className="container-skycons">
+                    <Skycons
+                        color="black"
+                        icon={iconText}
+                        autoplay={true}/>
+                </div>
                 <h4>{this.state.forecast.currently.summary}</h4>
                 <p>{Math.round(tempInCelsius)}&deg;C</p>
             </div>
